009bf33d63819c442541331846b079e21c2d4ed6
Ron Friedman <cryogen65@gmail.com>
2025-02-01 14:31:11 -0500

    add github routes for app auth

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,27 @@ const app = express();
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+// GitHub vars
+var gitHubStrategy = require("passport-github").Strategy;
+
+// Passport init
+passport.use(new gitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: "http://localhost:4000/auth/github/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  return cb(null, profile);
+}
+));
+
+// GitHub Auth Routes
+app.get("/auth/github", passport.authenticate("github", { failureRedirect: "/" }),
+  function(req, res) {
+    // If successful auth
+    res.redirect("/");
+  });
+
 // Handle requests by serving index.html for all routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
